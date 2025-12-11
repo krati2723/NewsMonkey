81dd3ea0f0a5a25ea679bc5b6935673a56061285
Kratika <kratikap665@gmail.com>
2025-12-11 23:31:39 +0530

    Adding Loading Spinner

--- src/components/News.js
+++ src/components/News.js
@@ -1,55 +1,64 @@
 import React, { Component } from 'react'
 import Newsitem from './Newsitem'
+import Spinner from './spinner'
+
 
 export class News extends Component {
   
 
   constructor(){
-      super();
+      super();   
       console.log("Hello I am a constructor from News component");
       this.state={
         articles: [],
         loading:false,
-        page:1
+        page:1,
+        totalResults:0
       };
        }
       async componentDidMount(){
         console.log("cdm");
-        let url="https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a31d8b20b8af482c9b00630c7b0ac70d&pageSiz=15";
+        let url=`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=a31d8b20b8af482c9b00630c7b0ac70d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        this.setState({loading:true});
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
-        this.setState({articles:parsedData.articles, totalArticles: parsedData.totalResults})
+        this.setState({articles:parsedData.articles, 
+          totalResults: parsedData.totalResults,
+          loading: false
+        
+        })
 
       }
        handlePrevClick=  async ()=>{
-         let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a31d8b20b8af482c9b00630c7b0ac70d&page=${this.state.page - 1}&pageSiz=15`;
+         let url=`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=a31d8b20b8af482c9b00630c7b0ac70d&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+         this.setState({loading:true});
         let data = await fetch(url);
         let parsedData = await data.json()
         console.log(parsedData);
        
           this.setState({
             page: this.state.page - 1,
-            articles:parsedData.articles
+            articles:parsedData.articles,
+            loading: false
           })
 
         }
         handleNextClick=  async()=>{
-          if(this.state.page +1 >Math.ceil(this.state.totalResults/15)){
-
-          }
-           else
-            {
-            let url=`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=a31d8b20b8af482c9b00630c7b0ac70d&page=${this.state.page + 1}&pageSiz=15`;
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        console.log(parsedData);
+          if(!(this.state.page +1 >Math.ceil(this.state.totalResults/this.props.pageSize))){
+          let url=`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=a31d8b20b8af482c9b00630c7b0ac70d&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+          this.setState({loading:true});
+           let data = await fetch(url);
+           let parsedData = await data.json()
+           
+           console.log(parsedData);
        
           this.setState({
             page: this.state.page +1,
-            articles:parsedData.articles
+            articles:parsedData.articles,
+            loading: false
           })
-        }
+          }
         }
      
  
@@ -58,11 +67,12 @@ export class News extends Component {
 
     return (
       <div className= "container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        {this.state.loading &&< Spinner/>}
        
         <div className="row">
-          {this.state.articles.map((element)=>{
-            return <div className="col-md-3" key={element.url}>
+          {!this.state.loading && this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url}>
               <Newsitem  title ={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageurl={element.urlToImage||element.image||"https://media.istockphoto.com/id/1264354259/photo/young-boy-playing-in-a-flag-football-game.jpg?s=612x612&w=0&k=20&c=ftLl-_yYfZj_YygO_rTcBxxSCNjY-pxe7Km-l2mUzEA="} newsurl={element.url}/>
               </div>
             })}
@@ -70,7 +80,7 @@ export class News extends Component {
         </div>
         <div className="container d-flex justify-content-between" >
           <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
-          <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          <button disabled={this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
         </div>
       </div>
     )
